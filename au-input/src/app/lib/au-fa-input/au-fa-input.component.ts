485da6e238012d1f113f9a9c0f55faeb432faeec
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2023-06-19 19:41:00 +0300

    create basic fa component

---
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "au-fa-input",
+  templateUrl: "./au-fa-input.component.html",
+  styleUrls: ["./au-fa-input.component.css"],
+})
+export class AuFaInputComponent implements OnInit {
+  @Input() icon: string | null = null;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  get classes() {
+    const cssClasses = {
+      fa: true,
+      [`fa-${this.icon}`]: !!this.icon,
+    };
+
+    return cssClasses;
+  }
+}
